21251b9ead834e6d2a9609809d8b1f8d851df067
jawc-05 <joaoalfredowc@gmail.com>
2025-09-27 14:06:29 -0300

    feat: deleting the old and useless components

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,13 @@
 import { useState } from 'react';
 
-import Profile from "./components/Profile";
 import Form from "./components/Form";
-import ReposList from './components/ReposList';
 import './App.css'
 
 function App() {
-  const [FormVisible, setFormVisible] = useState(true);
-  const [nameUser, setNameUser] = useState('');
-
   return(
-    <>
-    <h3>Coloque aqui embaixo o usuário do GitHub:</h3>
-    <input className='input-style' type="text" onBlur={(e) => setNameUser(e.target.value)} />
-
-      {nameUser.length > 4 &&(
-        <>
-          <Profile nameUser={nameUser} brand="FielFlow" />
-          <ReposList nameUser={nameUser} />
-        </>
-      )}
-
-    {/*FormVisible && (
-      <Form />
-    )}
-
-      <button onClick={() => setFormVisible(!FormVisible)} type='button'>toggle form</button> */}
-    </>
-  )
-}
+  <>
+    <Form></Form>
+  </>
+)};
 
 export default App
