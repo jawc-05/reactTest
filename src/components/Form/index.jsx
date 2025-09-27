bdee604cda12bd3fb7890d4fb4516dd762b602a8
jawc-05 <joaoalfredowc@gmail.com>
2025-09-27 14:06:56 -0300

    feat: adding Form component

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,10 +1,8 @@
 import { useState, useEffect, use } from "react";
 
 const Form= () => {
-    const [materiaA, setMateriaA] = useState(0);
-    const [materiaB, setMateriaB] = useState(0);
-    const [materiaC, setMateriaC] = useState(0);
-    const [name, setName] = useState("");
+    const [height, setHeight] = useState(0);
+    const [weight, setWeight] = useState(0);
 
     useEffect(() => {
         console.log("O componente foi montado")
@@ -13,16 +11,12 @@ const Form= () => {
             console.log("O componente foi desmontado")
         }
     }, []);
-    
-    useEffect(() => {
-        console.log("O nome foi alterado")
-    }, [name]);
 
     useEffect(() => {
         console.log("Materia A mudou para: " + materiaA )
     }, [materiaA, materiaB, materiaC]);
 
-    const changeName = (evento) => {
+    const changeHeight = (evento) => {
 
         setName(lastState => {
             console.log(lastState)
@@ -52,7 +46,7 @@ const Form= () => {
             <ul>
                 {[1,2,3,4,5].map(item => <li key={item} >{item}</li>)}
             </ul>
-            <input type="text" placeholder="Seu nome: " onChange={e => changeName(e)}/>
+            <input type="number" placeholder="Sua altura: " onChange={e => changeHeight(e)}/>
             <input type="number"  placeholder="Nota máteria A" onChange= {evento => setMateriaA(parseFloat(evento.target.value))} />
             <input type="number"  placeholder="Nota máteria B" onChange= {evento => setMateriaB(parseFloat(evento.target.value))}/>
             <input type="number"  placeholder="Nota máteria C" onChange= {evento => setMateriaC(parseFloat(evento.target.value))}/>
