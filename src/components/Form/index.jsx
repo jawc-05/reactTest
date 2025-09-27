8307fb72ac6f91aedac8dfd513a829ec92007a9a
jawc-05 <joaoalfredowc@gmail.com>
2025-09-27 14:19:50 -0300

    feat: adding calc function

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -28,19 +28,23 @@ const Form= () => {
 
     }
 
-    /*const renderizaResultado = () => {
-        const soma = (materiaA) + (materiaB) + (materiaC);
-        const media = soma / 3;
-        if (media >= 7) {
-            return(
-            <p>Olá! {name} você foi aprovado com média {media}</p>
-            )
-        } else {
-            return (
-            <p>Olá! {name} você foi reprovado com média {media}</p>
-        )
+    const renderizaResultado = () => {
+        const calc = (height) / (weight);
+
+        if (calc < 18.5) {
+            return <p>Abaixo do peso</p>
+        } if (calc >= 18.5 && calc < 24.9) {
+            return <p>Peso normal</p>
+        } if (calc >= 25 && calc < 29.9) {
+            return <p>Sobrepeso</p>
+        } if (calc >= 30 && calc < 34.9) {
+            return <p>Obesidade grau 1</p>
+        } if (calc >= 35 && calc < 39.9) {
+            return <p>Obesidade grau 2</p>
+        } if (calc >= 40) {
+            return <p>Obesidade grau 3</p>
         }
-    }*/
+    }
 
 
     return(
