edef64696a17bbdc1c937736a20690f2f6e00d73
jawc-05 <joaoalfredowc@gmail.com>
2025-09-27 14:12:31 -0300

    feat: adding features to form component

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -12,21 +12,23 @@ const Form= () => {
         }
     }, []);
 
-    useEffect(() => {
-        console.log("Materia A mudou para: " + materiaA )
-    }, [materiaA, materiaB, materiaC]);
-
     const changeHeight = (evento) => {
 
-        setName(lastState => {
+        setHeight(lastState => {
             console.log(lastState)
 
             return evento.target.value
         })
 
+        setWeight(lastState => {
+            console.log(lastState)
+
+            return evento.target.value
+                })
+
     }
 
-    const renderizaResultado = () => {
+    /*const renderizaResultado = () => {
         const soma = (materiaA) + (materiaB) + (materiaC);
         const media = soma / 3;
         if (media >= 7) {
@@ -38,19 +40,13 @@ const Form= () => {
             <p>Olá! {name} você foi reprovado com média {media}</p>
         )
         }
-    }
+    }*/
 
 
     return(
         <form>
-            <ul>
-                {[1,2,3,4,5].map(item => <li key={item} >{item}</li>)}
-            </ul>
             <input type="number" placeholder="Sua altura: " onChange={e => changeHeight(e)}/>
-            <input type="number"  placeholder="Nota máteria A" onChange= {evento => setMateriaA(parseFloat(evento.target.value))} />
-            <input type="number"  placeholder="Nota máteria B" onChange= {evento => setMateriaB(parseFloat(evento.target.value))}/>
-            <input type="number"  placeholder="Nota máteria C" onChange= {evento => setMateriaC(parseFloat(evento.target.value))}/>
-            {renderizaResultado()}
+            {/*renderizaResultado()*/}
         </form>
     )
 }
